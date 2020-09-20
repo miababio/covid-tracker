0a876c91c2fa462767f2dd90eb7ec20737323e08
Michael Ababio <michael.ababio@johnsonfit.com>
2020-09-20 16:13:55 -0500

    Rename interfaces to Typescript extension

--- src/interfaces/CardProps.tsx
+++
@@ -1,9 +0,0 @@
-interface CardProps {
-    styling: string;
-    cardTitle: string;
-    countUpValue: number;
-    lastUpdate: string;
-    cardDescription: string;
-};
-
-export default CardProps;
